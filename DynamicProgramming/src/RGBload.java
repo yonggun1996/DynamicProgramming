5ce0881bd1f2565328390c4e9a8aab778002bbdf
qkrdydrjs7@naver.com <qkrdydrjs7@naver.com>
2021-04-11 00:12:56 +0900

    백준 알고리즘 1149번 문제 입니다.(수정1)

--- DynamicProgramming/src/RGBload.java
+++ DynamicProgramming/src/RGBload.java
@@ -13,19 +13,19 @@ public class RGBload {
 			}
 		}
 		
-		int[][] sum = new int[N][3];
+		int[][] sum = new int[N][3];//앞서 칠했던 색과 비용을 합친 값을 담은 배열
 		sum[0][0] = cost[0][0];
 		sum[0][1] = cost[0][1];
 		sum[0][2] = cost[0][2];
 		
-		int line = 1;
+		int line = 1;//sum배열에 데이터를 삽입하기 위한 변수
 		while(line != N) {
-			for(int i = 0; i < 3; i++) {
-				int min = 2147483647;
+			for(int i = 0; i < 3; i++) {//sum배열에 데이터를 넣기 위한 for문
+				int min = 2147483647;//이전 집 비용중 최소비용을 구하기 위한 변수
 				
-				for(int j = 0; j < 3; j++) {
+				for(int j = 0; j < 3; j++) {//이전 집 비용중 최소비용을 확인하기 위한 for문
 					if(i != j) {
-						if(min > sum[line - 1][j]) {
+						if(min > sum[line - 1][j]) {//이전집과 중복되지 않으면서, 제일 작은값을 구한다
 							min = sum[line - 1][j];
 						}
 					}
@@ -37,7 +37,7 @@ public class RGBload {
 		}//while
 		
 		int answer = 2147483647;
-		for(int i = 0; i < 3; i++) {
+		for(int i = 0; i < 3; i++) {//마지막 집 까지 비용을 구할 때 가장 작은값을 구하는 for문
 			if(answer > sum[N - 1][i]) {
 				answer = sum[N - 1][i];
 			}
--- DynamicProgramming/src/RGBload_Buffer.java
+++ DynamicProgramming/src/RGBload_Buffer.java
@@ -1,4 +1,60 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class RGBload_Buffer {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[][] cost = new int[N][3];
+		
+		StringTokenizer st = null;
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < 3; j++) {
+				cost[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int[][] sum = new int[N][3];
+		sum[0][0] = cost[0][0];
+		sum[0][1] = cost[0][1];
+		sum[0][2] = cost[0][2];
+		
+		int line = 1;
+		while(line != N) {
+			for(int i = 0; i < 3; i++) {
+				int min = 2147483647;
+				
+				for(int j = 0; j < 3; j++) {
+					if(i != j) {
+						if(min > sum[line - 1][j]) {
+							min = sum[line - 1][j];
+						}
+					}
+				}//in-for
+				
+				sum[line][i] = min + cost[line][i];
+			}//out-for
+			line++;
+		}//while
+		
+		int answer = 2147483647;
+		for(int i = 0; i < 3; i++) {
+			if(answer > sum[N - 1][i]) {
+				answer = sum[N - 1][i];
+			}
+		}
+		
+		bw.write(String.valueOf(answer));
+		bw.flush();
+		bw.close();
+	}
 
 }
