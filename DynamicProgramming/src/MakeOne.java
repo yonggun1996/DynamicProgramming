8a9a39f7b5c9639d0a1facb4ab873904b995dcde
qkrdydrjs7@naver.com <qkrdydrjs7@naver.com>
2021-04-27 10:24:25 +0900

    백준 알고리즘 1463번 주석으로 문제 문제풀이 작성

--- DynamicProgramming/src/MakeOne.java
+++ DynamicProgramming/src/MakeOne.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class MakeOne {
 	
 	static int[] arr;
+	/* 어떠한 수가 주어지고 -1, /2, /3을 했을 때
+	 * 어떠한 방법이 1을 가장 빠르게 만들 수 있는가?
+	 * 
+	 * 1은 건드리지 않아도 1이기 때문에 0을 인덱스에 넣는다
+	 * 2부터 입력받은 숫자까지 어떠한 방법이 빠른지 그 횟수를 넣는다
+	 * 다만 숫자가 올라가면 올라갈수록 이전 숫자를 찾아와 연산횟수에 더해 속도를 빠르게 한다
+	 */
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
